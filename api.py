85fefcdfc0df5f07c80a4f166e3afa633020ef17
sesh <jwgforward@gmail.com>
2015-05-21 18:21:54 -0400

    factor out location resolution

--- api.py
+++ api.py
@@ -4,12 +4,25 @@ app = flask.Flask(__name__)
 
 iata = json.load(open('iata.json'))
 
+class ResolveException(Exception): pass
+def _resolve(location):
+    if location.upper() in iata:
+        return iata[location.upper()]
+    else:
+        # location must be a json lat/long pair
+        try:
+            return json.loads('[%s]' % location)
+        except Exception:
+            raise ResolveException()
+
 @app.route('/resolve/<string:location>')
 def resolve(location):
-    if location.upper() in iata:
-        return flask.Response(json.dumps({'location': iata[location.upper()]}), mimetype='application/json')
-    else:
-        try:
-            return flask.Response(json.dumps({'location': json.loads('[%s]' % location)}), mimetype='application/json')
-        except Exception:
-            return flask.Response(json.dumps({'error': 'invalid location: %s' % location}), status=500, mimetype='application/json')
+    try:
+        return flask.Response(json.dumps({'location': _resolve(location)}), mimetype='application/json')
+    except ResolveException:
+        return flask.Response(json.dumps({'error': 'invalid location: %s' % location}), status=500, mimetype='application/json')
+
+
+@app.route('/forecast/<src>/<dest>/<departure_datetime>/<speed_mph>/<time_step>')
+def forecast(src, dest, departure_datetime, speed_mph, time_step):
+    return flask.Response('not yet implemented')
